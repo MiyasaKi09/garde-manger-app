9d2c58e2c7c33c947045d8ee38998f101083e49b
MiyasaKi09 <42634222+MiyasaKi09@users.noreply.github.com>
2025-09-03 01:37:11 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,8 @@ export default function RootLayout({ children }) {
               <Link href="/pantry">Garde-manger</Link>
               <Link href="/recipes">Recettes</Link>
               <Link href="/garden">Potager</Link>
-              <Link href="/settings">Paramètres</Link>
+              <Link href="/settings">Paramètres</a>
+
             </nav>
           </div>
         </header>
