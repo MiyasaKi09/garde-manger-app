5dcc487596bce851152926280558065b3d73e5b4
MiyasaKi09 <42634222+MiyasaKi09@users.noreply.github.com>
2025-09-04 13:00:19 +0200

    Update route.js

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,16 +1,18 @@
+// app/auth/callback/route.js
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
-export async function GET(req: Request) {
+export async function GET(req) {
   const reqUrl = new URL(req.url);
   const code = reqUrl.searchParams.get('code');
   const redirect = reqUrl.searchParams.get('redirect') || '/';
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
-    // Échange le "code" contre une session persistée en cookies
+    // échange le code contre une session persistée en cookies
     await supabase.auth.exchangeCodeForSession(code);
   }
+
   return NextResponse.redirect(new URL(redirect, reqUrl.origin));
 }
