2b7562a85bf3d581786eeb29f3c5b80ea4034d98
MiyasaKi09 <42634222+MiyasaKi09@users.noreply.github.com>
2025-09-10 00:13:30 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,6 +32,7 @@ export default function RootLayout({ children }) {
       >
         <MinimalistHeader />
         <MatisseCutoutsBG />
+        <ReadableScrim /> 
 
         <main
           className="main-container"
