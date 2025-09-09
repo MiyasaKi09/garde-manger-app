adccad53bd60d47d66f7c65c351e65399be37a9f
MiyasaKi09 <42634222+MiyasaKi09@users.noreply.github.com>
2025-09-10 00:00:17 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,13 +22,13 @@ export default function RootLayout({ children }) {
       <body>
         <MinimalistHeader />
 
-<body style={{ background: '#0a0e0a' }}>
-  <MinimalistHeader />
-  <MatisseBackgroundStrong />
-  <main style={{ position:'relative', zIndex:1 }}>{children}</main>
-</body>
-
-            
+      <body style={{ background: '#0a0e0a' }}>
+        <MinimalistHeader />
+        <MatisseBackgroundStrong />
+        <main style={{ position:'relative', zIndex:1 }}>{children}</main>
+      </body>
+      
+                  
         {/* Pattern de fond organique subtil */}
         <div
           style={{
