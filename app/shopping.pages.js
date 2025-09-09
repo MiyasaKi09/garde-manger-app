136d19777ed3f2ed22c81efdb7168116c7e19f30
MiyasaKi09 <42634222+MiyasaKi09@users.noreply.github.com>
2025-09-09 23:32:31 +0200

    Create shopping.pages.js

---
+++ app/shopping.pages.js
@@ -0,0 +1,9 @@
+// app/shopping/page.js
+export default function ShoppingPage() {
+  return (
+    <div className="container">
+      <h1>🛒 Liste de courses</h1>
+      <p>Page en construction...</p>
+    </div>
+  );
+}
